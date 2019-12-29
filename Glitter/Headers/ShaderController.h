a5df437322659051a21fa2d0ec8d7e52886adc5b
danhuynh0803 <danhuynh0803@gmail.com>
2019-12-29 15:22:03 -0500

    Forgot to add new ShaderController class

---
+++ Glitter/Headers/ShaderController.h
@@ -0,0 +1,29 @@
+#ifndef SHADER_CONTROLLER_H
+#define SHADER_CONTROLLER_H
+
+#include "Shader.h"
+
+class ShaderController
+{
+public:
+    void ReloadShaders()
+    {
+        for (Shader* shader : shaderList)
+        {
+            // TODO Check that there was a change made to the file before reloading it
+            Shader newShader(shader->vertexName.c_str(), shader->fragName.c_str());
+
+            shader->ID = newShader.ID;
+        }
+    }
+
+    void Add(Shader* shader)
+    {
+        shaderList.push_back(shader);
+    }
+
+private:
+    std::vector<Shader*> shaderList;
+};
+
+#endif // SHADER_CONTROLLER_H
