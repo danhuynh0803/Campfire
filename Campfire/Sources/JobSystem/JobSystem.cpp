13b16039cd05180cfa108d85593ebeb58ce5c7e9
ewdlop <ray810815@gmail.com>
2020-06-28 01:38:34 -0400

    Add JobStealingQueue

--- Campfire/Sources/JobSystem/JobSystem.cpp
+++ Campfire/Sources/JobSystem/JobSystem.cpp
@@ -19,7 +19,11 @@ void JobSystem::Init()
     {
         for (int i = 0; i < numThreads; ++i)
         {
-            threadPool.push_back(std::thread(&JobSystem::Run, this));
+            jobStealingQueues.push_back(std::unique_ptr<JobStealingQueue<Job>>(new JobStealingQueue<Job>));
+        }
+        for (int i = 0; i < numThreads; ++i)
+        {
+            threadPool.push_back(std::thread(&JobSystem::JobThread, this,i));
         }
     }
     catch (...)
@@ -27,6 +31,55 @@ void JobSystem::Init()
         done = true;
         throw;
     }
+}
+
+void JobSystem::JobThread(unsigned index) {
+    myIndex = index;
+    localJobQueue = jobStealingQueues[myIndex].get();
+    while (!done) { return Run(); }
+}
+
+void JobSystem::Run()
+{
+    while (!done)
+    {
+        Job job;
+        if (PopJobFromLocalQueue(job)||
+            PopJobFromPoolQueue(job)||
+            PopJobOtherThreadQueue(job))
+        {
+            job();
+        }
+        else
+        {
+            std::this_thread::yield();
+        }
+    }
+}
+
+void JobSystem::Wait()
+{
+    while (!jobPoolQueue.Empty()) {}
+}
+
+bool JobSystem::PopJobFromLocalQueue(Job& job) {
+    return localJobQueue && localJobQueue->TryPop(job);
+}
+
+bool JobSystem::PopJobFromPoolQueue(Job& job) {
+    return jobPoolQueue.TryPop(job);
+}
+
+bool JobSystem::PopJobOtherThreadQueue(Job& job) {
+    for (unsigned i = 0; i < jobStealingQueues.size(); ++i)
+    {
+        unsigned const index = (myIndex + i + 1) % jobStealingQueues.size();
+        if (jobStealingQueues[index]->TrySteal(job))
+        {
+            return true;
+        }
+    }
+    return false;
 }
 
 bool JobSystem::HasWork()
@@ -38,41 +91,4 @@ bool JobSystem::HasWork()
 //        return true;
 //    }
     return false;
-}
-
-void JobSystem::Run()
-{
-    while (!done)
-    {
-        Job job;
-        if (jobQueue.TryPop(job))
-        {
-            //job.fn(job.args);
-            job();
-        }
-        else
-        {
-            std::this_thread::yield();
-        }
-    }
-}
-
-//template <typename FunctionType>
-//std::future<typename std::result_of<FunctionType()>::type>
-//    JobSystem::Submit(FunctionType f)
-//{
-//
-//}
-
-void JobSystem::Wait()
-{
-    // Wait until all tasks are complete
-    while (!jobQueue.Empty()) {}
-
-    //Moved to JobThreadsJoiner
-    //for (int i = 0; i < threadPool.size(); ++i)
-    //{
-    //    if (threadPool[i].joinable)
-    //        threadPool[i].join;
-    //}
 }
