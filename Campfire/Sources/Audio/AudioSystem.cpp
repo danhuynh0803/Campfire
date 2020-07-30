8a47b6a51c8a1f8f0ac7a67fe95e766cc80f6b9f
danhuynh0803 <danhuynh0803@gmail.com>
2020-07-29 22:34:48 -0400

    Audio system api set

--- Campfire/Sources/Audio/AudioSystem.cpp
+++ Campfire/Sources/Audio/AudioSystem.cpp
@@ -19,14 +19,14 @@ void AudioSystem::OnUpdate(float dt)
 {
 }
 
-void AudioSystem::PlaySound(const char* filepath)
+void AudioSystem::PlaySound(const std::string& filepath, const glm::vec3& pos, float volumedB)
 {
     FMOD::Channel *channel1 = 0, *channel2 = 0, *channel3 = 0;
 
     FMOD_VECTOR listenerPos = {0.0f, 0.0f, -1.0f};
 
     FMOD::Sound* bgm;
-    fmod->coreSystem->createSound(filepath, FMOD_DEFAULT, nullptr, &bgm);
+    fmod->coreSystem->createSound(filepath.c_str(), FMOD_DEFAULT, nullptr, &bgm);
     //bgm->setMode(FMOD_LOOP_NORMAL);
 
     fmod->coreSystem->playSound(bgm, 0, false, &channel1);
--- Campfire/Sources/Audio/AudioSystem.h
+++ Campfire/Sources/Audio/AudioSystem.h
@@ -5,6 +5,7 @@
 #include <map>
 #include <vector>
 #include <math.h>
+#include <glm/glm.hpp>
 
 #include "Core/Base.h"
 #include "Audio/FmodImpl.h"
@@ -16,8 +17,27 @@ public:
     static void Shutdown();
     static void OnUpdate(float dt);
 
-    static void AddSound();
-    static void PlaySound(const char* filepath);
+    // Studio related
+    static void LoadBank(const std::string& bankName, FMOD_STUDIO_LOAD_BANK_FLAGS flags);
+    static void LoadEvent(const std::string& eventName);
+    static void LoadSound(const std::string& soundName, bool is3d = true, bool isLooping = false, bool isStream = false);
+    static void UnloadSound(const std::string& soundName);
+
+    static void SetListenerPosition(const glm::vec3& pos = glm::vec3(0.0f), float volumedB = 0.0f);
+
+    static void PlaySound(const std::string& filepath, const glm::vec3& pos = glm::vec3(0.0f), float volumedB = 0.0f);
+    static void PlayEvent(const std::string& eventName, bool isImmediate = false);
+
+    static void StopAllChannels();
+    static void SetChannel3dPosition(int channelId, const glm::vec3& position);
+    static void SetChannelVolume(int channelId, float volumedB);
+
+    static bool IsPlaying(int channelId);
+    static bool IsEventPlaying(const std::string& eventName);
+    static float dBToVolume(float dB);
+    static float VolumeTodB(float volume);
+
+    static FMOD_VECTOR VectorToFmod(const glm::vec3& position);
 
 private:
     static UniquePtr<FmodImpl> fmod;
