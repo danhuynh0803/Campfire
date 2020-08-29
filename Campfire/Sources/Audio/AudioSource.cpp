6382736649b53e018f9d7c680e68e0e2a51eee25
danhuynh0803 <danhuynh0803@gmail.com>
2020-08-29 01:59:46 -0400

    initial commit on AudioSource

---
+++ Campfire/Sources/Audio/AudioSource.cpp
@@ -0,0 +1,21 @@
+#include "AudioSource.h"
+
+void AudioSource::Play()
+{
+}
+
+void AudioSource::Pause()
+{
+}
+
+void AudioSource::Unpause()
+{
+}
+
+void AudioSource::PlayDelayed(float delay)
+{
+}
+
+void AudioSource::Stop()
+{
+}
