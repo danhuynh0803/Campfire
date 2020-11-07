00ca051082c55a9dcfa50ea8650ee4895ca2a293
danhuynh0803 <danhuynh0803@gmail.com>
2020-11-07 02:04:50 -0500

    Buffer bar and spinner imgui widgets

--- Campfire/Sources/ImGui/ImGuiLayer.h
+++ Campfire/Sources/ImGui/ImGuiLayer.h
@@ -7,8 +7,16 @@
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
 #include <ImGuizmo.h>
+#include <imgui_internal.h>
 
 struct ImVec3 { float x, y, z; ImVec3(float _x = 0.0f, float _y = 0.0f, float _z = 0.0f) { x = _x; y = _y; z = _z; } };
+
+// Extra ImGui utility functions
+namespace ImGui
+{
+    bool BufferingBar(const char* label, float value, const ImVec2& size_arg, const ImU32& bg_col, const ImU32& fg_col);
+    bool Spinner(const char* label, float radius, int thickness, const ImU32& color);
+};
 
 class ImGuiLayer : public Layer
 {
