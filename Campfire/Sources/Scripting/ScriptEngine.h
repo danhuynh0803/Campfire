d6a5bdce5bd4b8f8fc64723eea474c39718da325
ewdlop <ray810815@gmail.com>
2020-11-08 15:23:47 -0500

    Lua scripting layer initialization

--- Campfire/Sources/Scripting/ScriptEngine.h
+++ Campfire/Sources/Scripting/ScriptEngine.h
@@ -1,16 +1,13 @@
 #ifndef SCRIPT_ENGINE_H
 #define SCRIPT_ENGINE_H
 
-#include "lua.hpp"
+#include "LuaScripting.h"
 
 class ScriptEngine
 {
 public:
-	ScriptEngine();
-	~ScriptEngine();
-
-private:
-
+	static void Init();
+	static void Shutdown();
 };
 
 #endif // !SCRIPT_ENGINE_H
