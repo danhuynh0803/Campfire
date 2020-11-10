6b0eeab4199ea9086c9260c614532795e3e12b81
danhuynh0803 <danhuynh0803@gmail.com>
2020-11-09 19:05:40 -0500

    Add Game application for release builds

---
+++ Game/src/GameApplication.cpp
@@ -0,0 +1,22 @@
+#include "Core/Application.h"
+#include "EntryPoint.h"
+#include "GameLayer.h"
+
+class GameApplication : public Application
+{
+public:
+    GameApplication(const ApplicationProps& props)
+        : Application(props)
+    {
+    }
+
+    virtual void OnInit() override
+    {
+        PushLayer(new GameLayer);
+    }
+};
+
+Application* CreateApplication()
+{
+    return new GameApplication({"Game", 1920, 1080});
+}
