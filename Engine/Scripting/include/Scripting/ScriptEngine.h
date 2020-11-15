71b43fe9333acff33c6c9acf0f89eee33968c6e3
ewdlop <ray810815@gmail.com>
2020-11-15 16:14:19 -0500

    Organize Lua Script Initialization

--- Engine/Scripting/include/Scripting/ScriptEngine.h
+++ Engine/Scripting/include/Scripting/ScriptEngine.h
@@ -3,14 +3,17 @@
 #include <lua.hpp>
 #include "Scripting/ArenaAllocator.h"
 #include <string>
+#include <typeinfo>
 
 class ScriptEngine
 {
 public:
-    static void RunFunction(const std::string& funcName, const std::string& scriptPath);
     static void Init();
+    static void RunFunction(const std::string& funcName, const std::string& scriptPath);
+    static void RunScript(const std::string& LuaScript);
     static void Shutdown();
 
 private:
     static lua_State* L;
+    static void Register();
 };
--- Engine/Scripting/src/ScriptEngine.cpp
+++ Engine/Scripting/src/ScriptEngine.cpp
@@ -23,57 +23,19 @@ void ScriptEngine::Init()
     char memory[POOL_SIZE];
     ArenaAllocator pool(memory, &memory[POOL_SIZE - 1]);
     L = lua_newstate(ArenaAllocator::l_alloc, &pool);
-    assert(L != nullptr);
+    L ? Register() : printf("Error: Fail to initialize Lua stack\n");
+}
 
-    /*{
-        auto NativePythagoras = [](lua_State* L) -> int
-        {
-            lua_Number a = lua_tonumber(L, -2);
-            lua_Number b = lua_tonumber(L, -1);
-            lua_Number csqr = (a * a) + (b * b);
-            lua_pushnumber(L, csqr);
-            return 1;
-        };
+void ScriptEngine::Register()
+{
+    if (!L) return;
+}
 
-        constexpr char* LUA_FILE = R"(
-        function Pythagoras( a, b )
-            csqr = NativePythagoras( a, b )
-            return csqr, a, b
-        end
-        )";
-
-        lua_State* L = luaL_newstate();
-        lua_pushcfunction(L, NativePythagoras);
-        lua_setglobal(L, "NativePythagoras");
-        luaL_dostring(L, LUA_FILE);
-        lua_getglobal(L, "Pythagoras");
-        if (lua_isfunction(L, -1))
-        {
-            lua_pushnumber(L, 3);
-            lua_pushnumber(L, 4);
-            constexpr int NUM_ARGS = 2;
-            constexpr int NUM_RETURNS = 3;
-            lua_pcall(L, NUM_ARGS, NUM_RETURNS, 0);
-            lua_Number c = lua_tonumber(L, -3);
-            printf("csqr = %d\n", (int)c);
-            lua_Number a = lua_tonumber(L, -2);
-            printf("a = %d\n", (int)a);
-            lua_Number b = lua_tonumber(L, -1);
-            printf("b = %d\n", (int)b);
-        }
-        lua_close(L);
-    }
-    {
-        lua_State* L = luaL_newstate();
-        luaL_openlibs(L);
-        luaL_dofile(L, ASSETS + "Scripts/test.lua");
-        lua_getglobal(L, "a");
-        if (lua_isnumber(L, -1))
-        {
-            int i = lua_tonumber(L, -1);
-            printf("b = %d\n", (int)i);
-        }
-    }*/
+void ScriptEngine::RunScript(const std::string& LuaScript)
+{
+    if (!L) return;
+    int doResult = luaL_dostring(L, LuaScript.c_str());
+    if (doResult != LUA_OK) printf("Error: %s\n", lua_tostring(L, -1));
 }
 
 void ScriptEngine::Shutdown()
