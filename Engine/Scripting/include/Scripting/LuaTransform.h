a805cf526400a1611dbd0da0371e8c571906fe0f
ewdlop <ray810815@gmail.com>
2020-11-16 00:04:49 -0500

    LuaTransfrom Setter/Getter

--- Engine/Scripting/include/Scripting/LuaTransform.h
+++ Engine/Scripting/include/Scripting/LuaTransform.h
@@ -4,20 +4,20 @@
 #include <functional>
 #include "Scene/Component.h"
 
-//static const struct luaL_reg {
-//    char* methodName;
-//    std::function<int(lua_State*)> method;
-//} transformLib[] =
-//{
-//    {"New", NewTransform},
-//    {"SetPosition", SetPosition},
-//    {"SetEuler", SetRotation},
-//    {"SetScale", SetScale},
-//
-//    {"GetPosition", GetPosition},
-//    {"GetEuler", GetRotation},
-//    {"GetScale", GetScale}
-//};
+static const struct luaL_reg {
+    char* methodName;
+    std::function<int(lua_State*)> method;
+} transformLib[] =
+{
+    {"New", NewTransform},
+    {"SetPosition", SetPosition},
+    {"SetEuler", SetRotation},
+    {"SetScale", SetScale},
+
+    {"GetPosition", GetPosition},
+    {"GetEuler", GetRotation},
+    {"GetScale", GetScale}
+};
 
 static int NewTransform(lua_State* L)
 {
@@ -41,27 +41,53 @@ static int SetPosition(lua_State* L)
 
 static int SetRotation(lua_State* L)
 {
+    TransformComponent* tc = (TransformComponent*)lua_touserdata(L, -4);
+    lua_Number x = lua_tonumber(L, -3);
+    lua_Number y = lua_tonumber(L, -2);
+    lua_Number z = lua_tonumber(L, -1);
+    tc->rotation = glm::quat(glm::vec3(
+        glm::radians(x),
+        glm::radians(y),
+        glm::radians(z)));
     return 0;
 }
 
 static int SetScale(lua_State* L)
 {
+    TransformComponent* tc = (TransformComponent*)lua_touserdata(L, -1);
+    lua_Number x = lua_tonumber(L, -3);
+    lua_Number y = lua_tonumber(L, -2);
+    lua_Number z = lua_tonumber(L, -1);
+    tc->scale = glm::vec3(x,y,z);
     return 0;
 }
 
 static int GetPosition(lua_State* L)
 {
-    return 0;
+    TransformComponent* tc = (TransformComponent*)lua_touserdata(L, -4);
+    lua_pushnumber(L, tc->position.x);
+    lua_pushnumber(L, tc->position.y);
+    lua_pushnumber(L, tc->position.z);
+
+    return 1;
 }
 
 static int GetRotation(lua_State* L)
 {
-    return 0;
+    TransformComponent* tc = (TransformComponent*)lua_touserdata(L, -1);
+    lua_pushnumber(L, tc->rotation.x);
+    lua_pushnumber(L, tc->rotation.y);
+    lua_pushnumber(L, tc->rotation.z);
+    return 1;
 }
 
 static int GetScale(lua_State* L)
 {
-    return 0;
+    TransformComponent* tc = (TransformComponent*)lua_touserdata(L, -1);
+    lua_pushnumber(L, tc->scale.x);
+    lua_pushnumber(L, tc->scale.y);
+    lua_pushnumber(L, tc->scale.z);
+    return 1;
 }
 
 static int ResetTransform(lua_State* L)
