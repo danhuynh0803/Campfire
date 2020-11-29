abccdd30afea40893b7da5810da978d77f93a904
danhuynh0803 <danhuynh0803@gmail.com>
2020-11-29 13:14:23 -0500

    Display contents of current directory

--- Editor/src/Widgets/AssetBrowser.cpp
+++ Editor/src/Widgets/AssetBrowser.cpp
@@ -12,7 +12,56 @@ AssetBrowser::AssetBrowser()
 
 void AssetBrowser::OnImGuiRender(bool* isOpen)
 {
+    static float scale = 1;
     ImGui::Begin("Assets", isOpen);
+
+    // Scaling size for icons
+    ImGui::SliderFloat("Icon Scale", &scale, 0.5f, 2.0f);
+
+    ImGui::SameLine();
+
+    if (ImGui::Button("Parent Dir"))
+    {
+        if (currPath != ASSETS)
+        {
+            currPath = currPath.parent_path();
+        }
+    }
+
+    uint32_t size = 40;
+    size *= scale;
+    ImVec2 buttonSize(size, size);
+
+    ImGuiStyle& style = ImGui::GetStyle();
+    float window_visible_x2 = ImGui::GetWindowPos().x + ImGui::GetWindowContentRegionMax().x;
+
+    int buttonCount = 0;
+    for (auto& p : std::filesystem::directory_iterator(currPath))
+    {
+        buttonCount++;
+    }
+
+    int n = 0;
+    for (auto& p : std::filesystem::directory_iterator(currPath))
+    {
+        ImGui::PushID(n);
+        std::string assetName = p.path().filename().string();
+        if (ImGui::Button(assetName.c_str(), buttonSize))
+        {
+            if (std::filesystem::is_directory(p.path())) {
+                currPath = std::filesystem::relative(p.path());
+            }
+        }
+        float last_button_x2 = ImGui::GetItemRectMax().x;
+        float next_button_x2 = last_button_x2 + style.ItemSpacing.x + buttonSize.x; // Expected position if next button was on same line
+        if (n + 1 < buttonCount && next_button_x2 < window_visible_x2)
+            ImGui::SameLine();
+        ImGui::PopID();
+
+        n++;
+    }
+
+
 
     ImGui::End();
 }
