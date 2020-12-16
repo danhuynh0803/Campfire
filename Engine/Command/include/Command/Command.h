54d435485505118ba488c179f8b07504767c93f8
ewdlop <ray810815@gmail.com>
2020-12-16 01:13:07 -0500

    Add Comment to ImGuiCommand

--- Engine/Command/include/Command/Command.h
+++ Engine/Command/include/Command/Command.h
@@ -13,6 +13,7 @@ public:
 };
 #endif //COMMAND_H
 
+//ImGui Command modifiy toggle value, between the new and the old, at the target reference directly.
 class ImGuiFloatCommand : public Command
 {
 public:
