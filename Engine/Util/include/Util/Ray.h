21815a790f14b1adf3ae2b693c1ac99247c8f74d
Danny Huynh <danhuynh0803@gmail.com>
2021-05-31 13:27:21 -0400

    Rewrite equation to more similary match the paper

--- Engine/Util/include/Util/Ray.h
+++ Engine/Util/include/Util/Ray.h
@@ -72,54 +72,33 @@ struct Ray
         // Intersection using Moller-Trumbore
         glm::vec3 E1 = b - a;
         glm::vec3 E2 = c - a;
-        glm::vec3 N = cross(E1, E2);
+        glm::vec3 P = glm::cross(mDir, E2);
 
-        float det = -glm::dot(mDir, N);
-        float invdet = 1.0 / det;
-        glm::vec3 AO = mOrig - a;
-        glm::vec3 DAO = glm::cross(AO, mDir);
+        float epsilon = 1e-6;
+        // Det = (D x E2) dot E1
+        float det = glm::dot(P, E1);
+        if (det > -epsilon && det < epsilon)
+            return false;
 
-        float u = glm::dot(E2, DAO) * invdet;
-        float v = -glm::dot(E1, DAO) * invdet;
-        t = glm::dot(AO, N) * invdet;
+        float invDet = 1.0 / det;
 
-        return (
-           det >= 1e-6
-          && t >= 0.0
-          && u >= 0.0
-          && v >= 0.0
-          && (u + v) <= 1.0
-        );
+        // Finding u,v,t with Cramer's rule
+        glm::vec3 T = mOrig - a;
+        float u = glm::dot(T, P) * invDet;
+        if (u < 0 || u > 1)
+            return false;
 
-        /*
-        // TODO: bug in this implementation
-        // Intersection using inside-outside test
-        // edges
-        glm::vec3 e0 = b - a;
-        glm::vec3 e1 = c - b;
-        glm::vec3 e2 = a - c;
+        glm::vec3 Q = glm::cross(T, E1);
+        float v = glm::dot(mDir, Q) * invDet;
+        if (v < 0 || u + v > 1)
+            return false;
 
-        glm::vec3 n = glm::cross(e0, -e2);
+        t = glm::dot(E2, Q) * invDet;
+        // Check if point is behind camera
+        if (t < 0)
+            return false;
 
-        // Find the distance that lies on triangle's normal
-        t = - glm::dot(n, mOrig) / glm::dot(n, mDir);
-        if (t < 0) return false; // behind camera
-
-        // point of intersection on the plane
-        glm::vec3 p = mOrig + mDir*t;
-
-        // Check that the point is within the triangle using inside-outside test
-        // inside the triangle of the point p is to the left of each edge
-        glm::vec3 c0 = glm::cross(e0, (p - a));
-        glm::vec3 c1 = glm::cross(e1, (p - b));
-        glm::vec3 c2 = glm::cross(e2, (p - c));
-
-        return
-               glm::dot(c0, n) > 0
-            && glm::dot(c1, n) > 0
-            && glm::dot(c2, n) > 0
-        ;
-        */
+        return true;
     }
 
     glm::vec3 mOrig, mDir;
